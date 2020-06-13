325d2dd1fd5b42e16af232cbca28c2ea4d4947b1
SafeyahShemali <safonorthly@gmail.com>
2020-06-13 04:50:20 +0300

    speedtest7 done

---
+++ speedtest-7/speedtest-7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speedtest-7
+//
+//  Created by Safeyah on 6/13/20.
+//  Copyright © 2020 kuwaitcodes.cohort2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
